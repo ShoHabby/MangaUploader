c1e543b850c80abf8368c278a95a4f6c926b0598
Christophe Savard <christophe_savard@hotmail.ca>
2025-05-24 01:45:48 -0400

    Project initial push

---
+++ MangaUploader.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MangaUploader.App.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ MangaUploader.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,5 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MangaUploader.App.ViewModels;
+
+public class ViewModelBase : ObservableObject { }
