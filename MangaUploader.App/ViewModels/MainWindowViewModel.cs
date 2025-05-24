329c36137f7f2736e80229ba9bfbeacef213ea0f
Christophe Savard <christophe_savard@hotmail.ca>
2025-05-24 03:11:32 -0400

    Adding settings file

--- MangaUploader.App/ViewModels/MainWindowViewModel.cs
+++ MangaUploader.App/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿namespace MangaUploader.ViewModels;
 
+/// <summary>
+/// Main window ViewModel
+/// </summary>
 public class MainWindowViewModel : ViewModelBase
 {
     public static string Greeting => "Welcome to Avalonia!";
--- MangaUploader.App/Views/MainWindow.axaml.cs
+++ MangaUploader.App/Views/MainWindow.axaml.cs
@@ -1,8 +1,32 @@
+using System;
 using Avalonia.Controls;
 
 namespace MangaUploader.Views;
 
+/// <summary>
+/// Main window view
+/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow() => InitializeComponent();
+
+    /// <inheritdoc />
+    protected override void OnOpened(EventArgs e)
+    {
+        base.OnOpened(e);
+
+        this.Width  = Settings.Default.Width;
+        this.Height = Settings.Default.Height;
+    }
+
+    /// <inheritdoc />
+    protected override void OnClosing(WindowClosingEventArgs e)
+    {
+        Settings.Default.Width  = (int)Math.Round(this.Width);
+        Settings.Default.Height = (int)Math.Round(this.Height);
+
+        Settings.Default.Save();
+
+        base.OnClosing(e);
+    }
 }
