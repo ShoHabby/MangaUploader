ae7a791e554341bd30c25b55e118f0e64fcc532e
Christophe Savard <christophe_savard@hotmail.ca>
2025-05-26 06:07:08 -0400

    Add entry converter

--- MangaUploader.Core/Models/Cubari/Entry.cs
+++ MangaUploader.Core/Models/Cubari/Entry.cs
@@ -1,6 +1,10 @@
-﻿namespace MangaUploader.Core.Models.Cubari;
+﻿using System.Text.Json.Serialization;
+using MangaUploader.Core.Converters;
+
+namespace MangaUploader.Core.Models.Cubari;
 
 /// <summary>
 /// Chapter entry model base class
 /// </summary>
+[JsonConverter(typeof(EntryConverter))]
 public abstract class Entry;
--- MangaUploader.Core/Models/Cubari/ProxyEntry.cs
+++ MangaUploader.Core/Models/Cubari/ProxyEntry.cs
@@ -1,9 +1,12 @@
-﻿namespace MangaUploader.Core.Models.Cubari;
+﻿using System.Text.RegularExpressions;
+using FastEnumUtility;
+
+namespace MangaUploader.Core.Models.Cubari;
 
 /// <summary>
 /// Proxy chapter entry model
 /// </summary>
-public sealed class ProxyEntry : Entry
+public sealed partial class ProxyEntry : Entry
 {
     /// <summary>
     /// Proxy types
@@ -11,8 +14,14 @@ public sealed class ProxyEntry : Entry
     public enum ProxyType
     {
         None,
-        ImageChest
+        ImgChest
     }
+
+    /// <summary>
+    /// Proxy entry regex match
+    /// </summary>
+    [GeneratedRegex(@"/proxy/api/([a-z]+)/chapter/([\w-]+)", RegexOptions.Compiled)]
+    private static partial Regex ProxyRegex { get; }
 
     /// <summary>
     /// Entry proxy type
@@ -22,4 +31,33 @@ public sealed class ProxyEntry : Entry
     /// Entry proxy ID
     /// </summary>
     public string ID { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Converts this ProxyEntry to a a Uri string
+    /// </summary>
+    /// <returns>The Uri string representation of this proxy entry</returns>
+    public string ToUri() => this.Type is not ProxyType.None ? $"/proxy/api/{this.Type.FastToString().ToLowerInvariant()}/chapter/{this.ID}" : string.Empty;
+
+    /// <summary>
+    /// Creates a ProxyEntry from a given uri string
+    /// </summary>
+    /// <param name="uri">Uri to create the entry from</param>
+    /// <returns>The created entry, or <see langword="null"/> if an entry could not be created</returns>
+    public static ProxyEntry? FromUri(string? uri)
+    {
+        if (string.IsNullOrEmpty(uri)) return null;
+
+        Match match = ProxyRegex.Match(uri);
+        if (match.Success && FastEnum.TryParse(match.Captures[1].ValueSpan, true, out ProxyType type))
+        {
+            return new ProxyEntry
+            {
+                Type = type,
+                ID   = match.Captures[2].Value
+            };
+        }
+
+        return null;
+
+    }
 }
