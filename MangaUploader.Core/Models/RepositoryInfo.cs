9bd6c43c95504754245884b6a5abb2d08d7ca096
Christophe Savard <christophe_savard@hotmail.ca>
2025-05-27 22:33:02 -0400

    Change binding for RepositoryInfo to not rely on ToString

--- MangaUploader.Core/Models/RepositoryInfo.cs
+++ MangaUploader.Core/Models/RepositoryInfo.cs
@@ -5,13 +5,4 @@
 /// </summary>
 /// <param name="Name">Repository name</param>
 /// <param name="ID">Repository ID</param>
-public readonly record struct RepositoryInfo(string Name, long ID)
-{
-    #region Overrides
-    /// <summary>
-    /// String representation of the Repository
-    /// </summary>
-    /// <returns></returns>
-    public override string ToString() => this.Name;
-    #endregion
-}
+public readonly record struct RepositoryInfo(string Name, long ID);
