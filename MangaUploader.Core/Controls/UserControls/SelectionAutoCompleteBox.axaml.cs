f25a9965d97c8cc271cef5c50439c3aeaf7808ef
Christophe Savard <christophe_savard@hotmail.ca>
2025-06-01 05:06:58 -0400

    Merge behaviour into new UserControl class

--- MangaUploader.Core/Controls/UserControls/SelectionAutoCompleteBox.axaml.cs
+++ MangaUploader.Core/Controls/UserControls/SelectionAutoCompleteBox.axaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
+using System.ComponentModel;
 using System.Windows.Input;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace MangaUploader.Core.Controls.UserControls;
 
@@ -69,6 +72,87 @@ public sealed partial class SelectionAutoCompleteBox : UserControl
     /// Control constructor
     /// </summary>
     public SelectionAutoCompleteBox() => InitializeComponent();
+    #endregion
+
+    #region Overrides
+    /// <inheritdoc />
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        this.AutoCompleteBox.KeyUp           += OnKeyUp;
+        this.AutoCompleteBox.GotFocus        += OnGotFocus;
+        this.AutoCompleteBox.LostFocus       += OnLostFocus;
+        this.AutoCompleteBox.DropDownOpening += OnDropDownOpening;
+    }
+
+    /// <inheritdoc />
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        this.AutoCompleteBox.KeyUp           -= OnKeyUp;
+        this.AutoCompleteBox.GotFocus        -= OnGotFocus;
+        this.AutoCompleteBox.LostFocus       -= OnLostFocus;
+        this.AutoCompleteBox.DropDownOpening -= OnDropDownOpening;
+    }
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Show the AutoCompleteBox dropdown
+    /// </summary>
+    private void ShowDropdown()
+    {
+        if (this.AutoCompleteBox is { IsDropDownOpen: true } or { SelectedItem: not null }) return;
+
+        this.AutoCompleteBox.PopulateDropDown(this.AutoCompleteBox, EventArgs.Empty);
+        this.AutoCompleteBox.OpeningDropDown(false);
+
+        if (this.AutoCompleteBox.IsDropDownOpen) return;
+
+        // We *must* set the field and not the property as we need to avoid the changed event being raised (which prevents the dropdown opening).
+        this.AutoCompleteBox._ignorePropertyChange = true;
+        this.AutoCompleteBox.SetCurrentValue(AutoCompleteBox.IsDropDownOpenProperty, true);
+    }
+    #endregion
+
+    #region Event Handlers
+    /// <summary>
+    /// KeyUp event handler
+    /// </summary>
+    /// <param name="sender">Event sender</param>
+    /// <param name="e">Event args</param>
+    private void OnKeyUp(object? sender, KeyEventArgs e) => ShowDropdown();
+
+    /// <summary>
+    /// GotFocus event handler
+    /// </summary>
+    /// <param name="sender">Event sender</param>
+    /// <param name="e">Event args</param>
+    private void OnGotFocus(object? sender, GotFocusEventArgs e) => ShowDropdown();
+
+    /// <summary>
+    /// LostFocus event handler
+    /// </summary>
+    /// <param name="sender">Event sender</param>
+    /// <param name="e">Event args</param>
+    private void OnLostFocus(object? sender, RoutedEventArgs e)
+    {
+        if (this.AutoCompleteBox.SelectedItem is null)
+        {
+            this.AutoCompleteBox.SetValue(AutoCompleteBox.TextProperty, string.Empty);
+        }
+    }
+
+    /// <summary>
+    /// DropDownOpening event handler
+    /// </summary>
+    /// <param name="sender">Event sender</param>
+    /// <param name="e">Event args</param>
+    private void OnDropDownOpening(object? sender, CancelEventArgs e)
+    {
+        if (this.AutoCompleteBox.TextBox is { IsReadOnly: true })
+        {
+            e.Cancel = true;
+        }
+    }
     #endregion
 }
 
